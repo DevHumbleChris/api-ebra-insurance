02550dacc4fae7e7919e74915e78897285f3dc0a
DevHumbleChris <dev.humblechris254@gmail.com>
2023-03-10 16:06:55 +0300

    added bad request exception

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { UserDTO } from './../dto/user.dto';
 import { PrismaService } from './../../prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +11,22 @@ export class AuthService {
 
   async signin(data: UserDTO) {
     const { email, password } = data;
+
+    // Check if user exists.
+    const user = await this.getUserByEmail(email);
+
+    if (!user) {
+      throw new BadRequestException("Email Doesn't Exits!");
+    }
+
     return 'Signed in routed';
+  }
+
+  async getUserByEmail(email: string) {
+    return await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
   }
 }
