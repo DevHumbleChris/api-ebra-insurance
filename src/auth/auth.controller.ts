d9fc87c2bc226e476dfb7a026eb16177224647f6
DevHumbleChris <dev.humblechris254@gmail.com>
2023-03-10 15:19:30 +0300

    added prisma service and prisma modeule

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,13 @@
+import { PrismaService } from './../../prisma/prisma.service';
 import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private prisma: PrismaService,
+  ) {}
 
   @Post('signup')
   async signup() {
