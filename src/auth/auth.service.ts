4edc1c6855bea44d426ace435ddea0a4aa9203a7
DevHumbleChris <dev.humblechris254@gmail.com>
2023-03-10 17:30:44 +0300

    modified the user model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { UserDTO } from './../dto/user.dto';
 import { PrismaService } from './../../prisma/prisma.service';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -16,7 +18,10 @@ export class AuthService {
       throw new BadRequestException('Email Already Exits!');
     }
 
+    // Hashed Password.
     const hashedPassword = await this.hashPassword(password);
+
+    // todo: Create new user.
 
     return hashedPassword;
   }
@@ -31,11 +36,21 @@ export class AuthService {
       throw new BadRequestException("Email Doesn't Exits!");
     }
 
+    // Check Password is correct.
+    const isPasswordCorrect = await this.checkPasswordMatch({
+      password,
+      hashedPassword: user.password,
+    });
+
+    if (!isPasswordCorrect) {
+      throw new BadRequestException('Password is incorrect!')
+    }
+
     return 'Signed in routed';
   }
 
   // Get User By Email.
-  async getUserByEmail(email: string) {
+  async getUserByEmail(email: string): Promise<User | null | undefined> {
     return await this.prisma.user.findUnique({
       where: {
         email,
@@ -44,8 +59,31 @@ export class AuthService {
   }
 
   // Hash Password.
-  async hashPassword(password) {
+  async hashPassword(password): Promise<string> {
     const saltRounds = 10;
     return await bcrypt.hash(password, saltRounds);
+  }
+
+  // Check if password matches.
+  async checkPasswordMatch(args: {
+    password: string;
+    hashedPassword: string;
+  }): Promise<boolean> {
+    return await bcrypt.compare(args.password, args.hashedPassword);
+  }
+
+  // Create New User.
+  async createNewUser(args: {
+    email: string;
+    password: string;
+    accessToken: string;
+  }): Promise<User> {
+    return await this.prisma.user.create({
+      data: {
+        email: args.email,
+        password: args.password,
+        accessToken: args.accessToken,
+      },
+    });
   }
 }
